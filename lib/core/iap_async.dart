134147c0350f57ec14bdb24dbfebffaf10ccf5bc
enuui <k.fuhui.c@gmail.com>
2024-01-26 16:37:48 +0800

    refactor: async

---
+++ lib/core/iap_async.dart
@@ -0,0 +1,172 @@
+import 'dart:async';
+
+import 'package:store_kit_iap/core/data/model.dart';
+import 'package:store_kit_iap/core/ut.dart';
+
+import 'data/result.dart';
+import 'iap.dart';
+import 'iap_callback.dart';
+
+class StoreKitAsync {
+  StoreKitAsync() {
+    _storeKit.addListener(_callback);
+  }
+
+  final StoreKit _storeKit = StoreKit();
+  final _callback = _StoreKitIapCallback();
+
+  /// 是否有资格获得试用优惠
+  Future<bool> eligibleForIntroOffer(String productId) {
+    final task = _callback.newTask<bool>();
+
+    _storeKit
+        .eligibleForIntroOffer(productId, requestId: task.requestId)
+        .catchError(task.error);
+
+    return task.future;
+  }
+
+  Future<void> getProduct(String productId) {
+    final task = _callback.newTask<bool>();
+
+    _storeKit
+        .getProduct(productId, requestId: task.requestId)
+        .catchError(task.error);
+
+    return task.future;
+  }
+
+  Future<void> finish(int transactionId) {
+    return _storeKit.finish(transactionId);
+  }
+
+  Future<String> vendorId() {
+    return _storeKit.vendorId();
+  }
+
+  /// 支付
+  Future<Transaction> purchase(PurchaseOpt opt) {
+    final task = _callback.newTask<Transaction>();
+
+    _storeKit.purchase(opt, requestId: task.requestId).catchError(task.error);
+
+    return task.future;
+  }
+
+  /// 当前的权益序列会发出用户拥有权益的每个产品的最新交易，具体包括：
+  /// - 每个非消耗性应用内购买的交易
+  /// - 每个自动续订订阅的最新交易，其Product.SubscriptionInfo.RenewalState状态为subscribed或inGracePeriod
+  /// - 每个非续订订阅的最新交易，包括已完成的订阅
+  /// - App Store退款或撤销的产品不会出现在当前的权益中。消耗性应用内购买也不会出现在当前的权益中。
+  /// Important: 要获取未完成的消耗性产品的交易，请使用Transaction中的unfinished或all序列。
+  Future<List<Transaction>> current({String requestId = ''}) {
+    final task = _callback.newTask<List<Transaction>>();
+
+    _storeKit.current(requestId: task.requestId).catchError(task.error);
+
+    return task.future;
+  }
+
+  /// 当前的权益序列，例如询问购买交易、订阅优惠码兑换以及客户在App Store中进行的购买。
+  /// 它还会发出在另一台设备上完成的客户端在您的应用程序中的交易。
+  Future<List<Transaction>> updates() {
+    final task = _callback.newTask<List<Transaction>>();
+
+    _storeKit.updates(requestId: task.requestId).catchError(task.error);
+
+    return task.future;
+  }
+
+  /// 需要处理的交易。未处理的交易会在启动时的 updates 中返回
+  Future<List<Transaction>> unfinished() {
+    final task = _callback.newTask<List<Transaction>>();
+
+    _storeKit.unfinished(requestId: task.requestId).catchError(task.error);
+
+    return task.future;
+  }
+
+  /// 所有的交易
+  /// Important: 要获取未完成的消耗性产品的交易，请使用Transaction中的unfinished或all序列。
+  Future<List<Transaction>> all() {
+    final task = _callback.newTask<List<Transaction>>();
+
+    _storeKit.all(requestId: task.requestId).catchError(task.error);
+
+    return task.future;
+  }
+}
+
+class _Task<T> {
+  final _completer = Completer<T>();
+  final String requestId;
+
+  _Task(this.requestId);
+
+  void complete(Result<T> result) {
+    if (result.requestId != requestId) return;
+    if (_completer.isCompleted) return;
+
+    if (result.error != null) {
+      _completer.completeError(result.error!);
+    } else {
+      _completer.complete(result.data);
+    }
+  }
+
+  Future<T> get future => _completer.future;
+
+  void error(Object error) => _completer.completeError(error);
+}
+
+class _StoreKitIapCallback implements StoreKitIapCallback {
+  final _callbacks = <String, _Task>{};
+
+  _Task<T> newTask<T>() {
+    final requestId = uuid();
+    final task = _Task<T>(requestId);
+    _callbacks[requestId] = task;
+    return task;
+  }
+
+  void notify<T>(Result<T> r) {
+    final completer = _callbacks.remove(r.requestId);
+    if (completer == null) return;
+    completer.complete(r);
+  }
+
+  @override
+  void all(Result<List<Transaction>> result) {
+    notify(result);
+  }
+
+  @override
+  void current(Result<List<Transaction>> result) {
+    notify(result);
+  }
+
+  @override
+  void eligibleCallback(Result<bool> result) {
+    notify(result);
+  }
+
+  @override
+  void productCallback(Result<Map<String, dynamic>> result) {
+    notify(result);
+  }
+
+  @override
+  void purchase(Result<Transaction> result) {
+    notify(result);
+  }
+
+  @override
+  void unfinished(Result<List<Transaction>> result) {
+    notify(result);
+  }
+
+  @override
+  void updates(Result<List<Transaction>> result) {
+    notify(result);
+  }
+}
--- lib/store_kit_iap.dart
+++ lib/store_kit_iap.dart
@@ -1,5 +1,6 @@
 library store_kit_iap;
 
 export 'core/iap.dart';
+export 'core/iap_async.dart';
 export 'core/iap_callback.dart';
 export 'core/data/model.dart';
